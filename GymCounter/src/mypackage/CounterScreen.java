081fc5701f9e86b890ee69d4c491bd5902177f93
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2023-10-12 12:50:52 -0400

    commented the code

--- GymCounter/src/mypackage/CounterScreen.java
+++ GymCounter/src/mypackage/CounterScreen.java
@@ -17,89 +17,118 @@ import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.ui.container.VerticalFieldManager;
 
 public class CounterScreen extends MainScreen {
-	
+	//declare public variables...
 	public int setCount = 0;
 	public int repsInput = 0;
 	public LabelField setsLbl = new LabelField();
 	public LabelField totalRepsLbl = new LabelField();
+	public LabelField stopwatchLbl = new LabelField();
 	public ButtonField addSet = new ButtonField(ButtonField.CONSUME_CLICK);
 	public ButtonField undoSet = new ButtonField(ButtonField.CONSUME_CLICK);
 	
 	public CounterScreen(int reps) {
-		super(FullScreen.VERTICAL_SCROLL | FullScreen.VERTICAL_SCROLLBAR);
+		//set screen title
 		setTitle("Gym Counter");
+
+		//set public reps variable to reps integer transfered to this screen
 		repsInput = reps;
+		
+		//create vertical field manager for content and set padding
 		VerticalFieldManager counterContent = new VerticalFieldManager();
 		counterContent.setPadding(15, 15, 15, 15);
 		
+		//create label to display reps per set
 		LabelField repsLbl = new LabelField();
+
+		//set label text, convert reps integer to string
 		repsLbl.setText("Reps per set: " + String.valueOf(reps));
 		
+		//set total reps label (the combined amount of reps, reps * sets)
 		totalRepsLbl.setText("Total Reps: 0");
 		
+		//set sets label to 0
 		setsLbl.setText("Sets: 0");
+
 		try {
-	        FontFamily alphaSerifFamily;
-			alphaSerifFamily = FontFamily.forName("BBSansSerifSquare");
-			Font setFont = alphaSerifFamily.getFont(Font.BOLD, 14, Ui.UNITS_pt);
-			Font totalRepsFont = alphaSerifFamily.getFont(Font.BOLD, 10, Ui.UNITS_pt);
-	        setsLbl.setFont(setFont);
-	        totalRepsLbl.setFont(totalRepsFont);
+	        FontFamily alphaSerifFamily; //create font family
+			alphaSerifFamily = FontFamily.forName("BBSansSerifSquare"); //get font family loaded on OS, you know the drill
+			Font mainFont = alphaSerifFamily.getFont(Font.BOLD, 14, Ui.UNITS_pt); //main font; bold, 14pt
+			Font secondaryFont = alphaSerifFamily.getFont(Font.BOLD, 10, Ui.UNITS_pt); //secondary font; bold, 10pt
 	        
+			//apply fonts...
+			setsLbl.setFont(mainFont);
+	        totalRepsLbl.setFont(secondaryFont);
+			stopwatchLbl.setFont(mainFont);	        
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			//catch exception and log it
 			e.printStackTrace();
 		} 
 			
+		//create horizontal field manager to display buttons side-by-side
 		HorizontalFieldManager buttonList = new HorizontalFieldManager();
 		
-		FieldChangeListener addListenere = new FieldChangeListener() {
+		//handle add button press...
+		FieldChangeListener addListener = new FieldChangeListener() {
             public void fieldChanged(Field field, int context) {
-            	if (setCount < 100)
+            	if (setCount < 101)
             	{
-            		setCount++;
-            		setsLbl.setText("Sets: " + String.valueOf(setCount));
-            		int totalReps = setCount * repsInput;
-            		totalRepsLbl.setText("Total Reps: " + String.valueOf(totalReps));
+					//if fewer than 101 sets (which is way higher than needed), execute code below
+            		setCount++; //+1 set
+            		setsLbl.setText("Sets: " + String.valueOf(setCount)); //update setsLbl to reflect current amount of sets
+            		int totalReps = setCount * repsInput; //calculate total reps
+            		totalRepsLbl.setText("Total Reps: " + String.valueOf(totalReps)); //update total reps label
             		
+					//enable undo button
             		undoSet.setEnabled(true);
             	}
             }
         };
 
+		//handle undo button press...
         FieldChangeListener undoListener = new FieldChangeListener() {
             public void fieldChanged(Field field, int context) {
             	if (setCount > 0)
             	{
-            		setCount--;
+					//if more than 0 sets, execute code below...
+            		setCount--; //decrease set count by one
             		if (setCount == 0)
             		{
+						//if setCount is equal to zero, disable the undo button (we don't want -1 sets!)
             			undoSet.setEnabled(false);
             		}
 
-            		setsLbl.setText("Sets: " + String.valueOf(setCount));
-            		int totalReps = setCount * repsInput;
-            		totalRepsLbl.setText("Total Reps: " + String.valueOf(totalReps));
+            		setsLbl.setText("Sets: " + String.valueOf(setCount)); //update setsLbl to reflect current amount of sets
+            		int totalReps = setCount * repsInput; //calculate total reps
+            		totalRepsLbl.setText("Total Reps: " + String.valueOf(totalReps)); //update total reps label
             	}
             }
         };
 
+		//set add button text and assign listener
 		addSet.setLabel("+1 Set");
-		addSet.setChangeListener(addListenere);
+		addSet.setChangeListener(addListener);
 		
+		//set undo button text and assign listener
 		undoSet.setLabel("Undo Add");
 		undoSet.setChangeListener(undoListener);
-		undoSet.setEnabled(false);
+		undoSet.setEnabled(false); //disable undo button by default
 		
+		//add buttons to our horizontal field manager
 		buttonList.add(addSet);
 		buttonList.add(undoSet);
 		
+		//top separator; 15px margin top
 		SeparatorField topSeparator = new SeparatorField();
 		topSeparator.setMargin(15, 0, 0, 0);
 		
+		//bottom separator; 15px margin bottom
 		SeparatorField bottomSeparator = new SeparatorField();
 		bottomSeparator.setMargin(0, 0, 15, 0);
+
+		//set stopwatch label text to default (0 minutes, 0 seconds)
+		stopwatchLbl.setText("00:00");
 		
+		//add all the stuff above to our vertical field manager...
 		counterContent.add(repsLbl);
 		counterContent.add(topSeparator);
 		counterContent.add(setsLbl);
@@ -107,12 +136,11 @@ public class CounterScreen extends MainScreen {
 		counterContent.add(bottomSeparator);
 		counterContent.add(buttonList);
 
-		
-		add(counterContent);
-		
-		
+		//add our vertical field manager to the screen
+		add(counterContent);		
 	}
 
+	//intercept back button press...
     public boolean onClose() 
     {
     	//close screen if user confirms when prompted...
@@ -127,8 +155,20 @@ public class CounterScreen extends MainScreen {
     	return true;
     }
 
-    private MenuItem restartSet = new MenuItem("Restart", 100, 10){
-        public void run(){
+	//custom menu item for start/stop stopwatch
+    private MenuItem stopwatchControl = new MenuItem("Start/Stop", 0, 0)
+	{
+        public void run()
+		{
+        	//TODO: add le code & rename "mypackage"
+        }
+    };  
+
+	//custom menu item for restart
+    private MenuItem restartSet = new MenuItem("Restart", 0, 0)
+	{
+        public void run()
+		{
         	//reset sets and reps values if user confirms when prompted...
         	int ans =  Dialog.ask(Dialog.D_YES_NO, "Are you sure you that you want to restart? Your progress will be lost.", Dialog.YES);
 
@@ -142,8 +182,11 @@ public class CounterScreen extends MainScreen {
         }
     };  
     
-    private MenuItem closeScreen = new MenuItem("Back", 100, 10){
-        public void run(){
+	//custom menu item for closing screen
+    private MenuItem closeScreen = new MenuItem("Back", 0, 0)
+	{
+        public void run()
+		{
         	//close screen if user confirms when prompted...
         	int ans =  Dialog.ask(Dialog.D_YES_NO, "Are you sure you that you want to go back? Your progress will be lost.", Dialog.YES);
 
@@ -155,8 +198,11 @@ public class CounterScreen extends MainScreen {
         }
     };  
     
-    protected void makeMenu(Menu menu, int instance){
-    	menu.add(restartSet);
+    protected void makeMenu(Menu menu, int instance)
+	{
+        //add custom menu items and separator to distinguish from system menu items
+    	menu.add(stopwatchControl);
+		menu.add(restartSet);
         menu.add(closeScreen);
         menu.addSeparator();
     }
--- GymCounter/src/mypackage/StartScreen.java
+++ GymCounter/src/mypackage/StartScreen.java
@@ -17,74 +17,96 @@ import net.rim.device.api.ui.component.Menu;
 import net.rim.device.api.ui.component.SeparatorField;
 import net.rim.device.api.ui.text.NumericTextFilter;
 
-/**
- * A class extending the MainScreen class, which provides default standard
- * behavior for BlackBerry GUI applications.
- */
 public final class StartScreen extends MainScreen
 {
-    /**
-     * Creates a new StartScreen object
-     */
-    public EditField repField = new EditField();
+    public EditField repField = new EditField(); //text input field for entering number of reps
     
     public StartScreen()
     {        
-        // Set the displayed title of the screen       
+        //set screen title    
         setTitle("Gym Counter");
+
+        //create vertical field manager (basically a layout that displays items top-down)
         VerticalFieldManager mainField = new VerticalFieldManager();
-        		
+        
+        //set padding for more polished look
         mainField.setPadding(15, 15, 15, 15);
+
+        //create label for the title of the app, set text to "Gym Counter"
         LabelField mainTitle = new LabelField();
         mainTitle.setText("Gym Counter");
+
+        //attempt to load font that comes with OS
 		try {
-	        FontFamily alphaSerifFamily;
-			alphaSerifFamily = FontFamily.forName("BBSansSerifSquare");
-			Font titleFont = alphaSerifFamily.getFont(Font.BOLD, 18, Ui.UNITS_pt);
-	        mainTitle.setFont(titleFont);
+	        FontFamily alphaSerifFamily; //create font family variable
+			alphaSerifFamily = FontFamily.forName("BBSansSerifSquare"); //load "BBSansSerifSquare" font family
+			Font titleFont = alphaSerifFamily.getFont(Font.BOLD, 18, Ui.UNITS_pt); //create font. bold, 18pt
+	        mainTitle.setFont(titleFont); //set title font
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			//catch exception if font not available
 			e.printStackTrace();
 		} 
-        LabelField mainCaption = new LabelField();
+
+        //create label for caption. essentially tells user the purpose of this app
+        LabelField mainCaption; //TEST THIS
         mainCaption.setText("Keep track of your progress at the gym!");
+
+        //create separator with top and bottom margin of 15px
+        SeparatorField separator = new SeparatorField();
+        separator.setMargin(15, 0, 15, 0);
         
+        //create label for input description
         LabelField startTxt = new LabelField();
         startTxt.setText("Enter the number of reps per set below...");
         
+        //set the publicly-declared "repField" variable to only accept numbers and be labeled as "Reps: "
         NumericTextFilter numericFilter = new NumericTextFilter();
         repField.setFilter(numericFilter);
         repField.setLabel("Reps: ");
         
-        FieldChangeListener listener = new FieldChangeListener() {
+        //handle "Start" button press (see below)
+        FieldChangeListener startListener = new FieldChangeListener() {
             public void fieldChanged(Field field, int context) {
+                //try catch block in case input is out of bounds
             	try
             	{
+                    //convert text input to integer from string
                 	int repCount = Integer.parseInt(repField.getText());
+
+                    //create new screen
 	            	CounterScreen counterScreen = new CounterScreen(repCount);
+
+                    //push screen to stack
 	            	Ui.getUiEngine().pushScreen(counterScreen);
             	}
             	catch (NumberFormatException e)
             	{
+                    //if invalid input exception is thrown, show a dialog telling the user to correct their error
             		Dialog.alert("Please enter a number of reps.");
             	}
             }
         };
 
+        //create button field
+        //CONSUME_ClICK is important because it prevents quick menu from showing up when button is clicked
         ButtonField startBtn = new ButtonField(ButtonField.CONSUME_CLICK);
+
+        //set label and margin
         startBtn.setLabel("Start Workout");
         startBtn.setMargin(15, 0, 0, 0);
-        startBtn.setChangeListener(listener);
 
-        SeparatorField separator = new SeparatorField();
-        separator.setMargin(15, 0, 15, 0);
+        //handle clicks by executing field listener above
+        startBtn.setChangeListener(startListener);
         
+        //add fields to vertical field manager...
         mainField.add(mainTitle);
         mainField.add(mainCaption);
         mainField.add(separator);
         mainField.add(startTxt);
         mainField.add(repField);
         mainField.add(startBtn);
+
+        //add vertical field manager to screen
         add(mainField);
     }
     
@@ -94,15 +116,20 @@ public final class StartScreen extends MainScreen
     	this.close();
     	return true;
     }
-    
-    private MenuItem aboutApp = new MenuItem("About", 100, 10){
-        public void run(){
-            // insert code thats to be done when menu is selected here here
+     
+    //create menu item titled "About"
+    private MenuItem aboutApp = new MenuItem("About", 0, 0)
+    {
+        public void run()
+        {
+            //on click, show about info dialog
         	Dialog.alert("Gym Counter\nVersion 1.0.0\n\nCreated by John Spahr\n\njohnspahr.org\nlunarproject.org");
         }
     };  
     
-    protected void makeMenu(Menu menu, int instance){
+    protected void makeMenu(Menu menu, int instance)
+    {
+        //add custom menu items and separator to distinguish from system menu items
         menu.add(aboutApp);
         menu.addSeparator();
     }
